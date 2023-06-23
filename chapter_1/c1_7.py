926bbd5d96cc880fdedb20bcb07210ee88331863
jiantaosu <jiantaosu9@hotmail.com>
2023-06-23 21:15:20 +0800

    update

---
+++ chapter_1/c1_7.py
@@ -0,0 +1,11 @@
+# 字典排序
+from collections import OrderedDict
+
+d = OrderedDict()
+d['foo'] = 1
+d['bar'] = 2
+d['spam'] = 3
+d['grok'] = 4
+
+for key in d:
+    print(key, d[key])
---
+++ chapter_1/c1_8.py
@@ -0,0 +1,12 @@
+# 字典的运算
+prices = {
+    'ACME': 45.23,
+    'AAPL': 612.78,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
+}
+
+min_price = min(zip(prices.values(), prices.keys()))
+
+print(min_price)
